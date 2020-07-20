260189b8041c0c6b358126f170527f5268b4612d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-07-20 23:20:17 +0900

    Add a test for store_cache()

--- tests/test_persistent_cache.py
+++ tests/test_persistent_cache.py
@@ -1,6 +1,6 @@
 import pytest
 
-from persistent_cache import persistent_cache
+from persistent_cache import persistent_cache, store_cache
 
 import time
 
@@ -23,6 +23,41 @@ def test_persistent_cache(tmpdir):
     # With a cache
     start = time.monotonic()
     assert heavy_computation(21) == 42
+    end = time.monotonic()
+
+    # This time it must finish much faster
+    assert end - start < 1
+
+def test_store_cache(tmpdir):
+    tmpdir.chdir()
+
+    @persistent_cache
+    def heavy_computation(x):
+        time.sleep(1)
+        return x * 2
+
+    # Store a value as a cache
+    store_cache(42, heavy_computation, 21)
+
+    # With a cache
+    start = time.monotonic()
+    assert heavy_computation(21) == 42
+    end = time.monotonic()
+
+    # This time it must finish much faster
+    assert end - start < 1
+
+    # Without a cache
+    start = time.monotonic()
+    assert heavy_computation(10) == 20
+    end = time.monotonic()
+
+    # Computation must take more than 1 second
+    assert end - start >= 1
+
+    # With a cache
+    start = time.monotonic()
+    assert heavy_computation(10) == 20
     end = time.monotonic()
 
     # This time it must finish much faster
